8803ee5caf55d2e448063ed18cb2445fcacb9e6e
Nilss98 <breeman.nb@gmail.com>
2021-06-02 14:46:09 +0200

    add Car class

---
+++ code/classes/Car.py
@@ -0,0 +1,21 @@
+
+class Car():
+
+    def __init__(self, car_id, orientation, column, row, length):
+        self.car_id = car_id
+        self.orientation = orientation
+        self.column = column
+        self.row = row
+        self.length = length
+
+    def load_car():
+        pass
+
+    def move_car():
+        pass
+
+    def is_valid():
+        pass
+
+    def get_possibilities():
+        pass
