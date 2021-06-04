52ad2b03de2c43d0b1bfe69deeb7c754feadc93a
Nilss98 <breeman.nb@gmail.com>
2021-06-04 16:08:23 +0200

    changes

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ from code.output import output
 def main():
     # rushHourBoard = Board('./data/input/Rushhour6x6_1.csv')
 
-    counters = 0
-    maximum = 0 
-    for i in range(100):
-        rushHourBoard = Board('./data/input/Rushhour6x6_1.csv')
-        counter = randomise.run(rushHourBoard)
-        if counter > maximum:
-            maximum = counter
+    total_moves = 0
+    max_moves = 0
 
-        counters += counter
+    for i in range(1000):
+        rushHourBoard = Board('./data/input/Rushhour6x6_3.csv')
+        move_count = randomise.run(rushHourBoard)
+        total_moves += move_count
         
-    print(f"The average amount of steps of 100 iterations is: {counters/100}")
-    print(f"The maximum steps of 100 iterations is: {maximum}")
+        if move_count > max_moves:
+            max_moves = move_count
+        
+    # print(f"The average amount of steps of 100 iterations is: {counters/100}")
+    # print(f"The maximum steps of 100 iterations is: {maximum}")
+    print(f'avg of 1000 was:\n    {total_moves/1000}\nmax number of steps was:\n    {max_moves}')
+
     # all_moves = randomise.run(rushHourBoard)
 
     # output.export_to_csv(all_moves, './data/output/output.csv')
