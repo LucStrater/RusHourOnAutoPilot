c4f53fb0df9c95c6cc89db4f8a6f8228f4fe729a
mees-wortelboer <meesmaurits@hotmail.com>
2021-06-04 13:14:32 +0200

    Added finishing move algorithm

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -1,58 +1,73 @@
 import random
+from code.algorithms import randomise
 
 
-def random_move(board):
-    """
-    Pick random car and make a legal move.
-    """
-    random_car_possibilities = []
 
-    while len(random_car_possibilities) == 0:
-        # pick random car
-        random_car = random.choice(list(board.cars.values()))
+# def red_car(board):
+#     """
+#     Check if red car can be moved right.
+#     """
+#     red_car = board.cars['X']
+#     possibilities = red_car.get_possibilities(board)
 
-        # get possibilities
-        random_car_possibilities = random_car.get_possibilities(board)
-
-    # make random move
-    random_move = random.choice(random_car_possibilities)
-    board.update_matrix(random_move, random_car)
+#     if len(possibilities) > 0:
+#         for i in range(len(possibilities)):
+#             if possibilities[i] > 0:
+#                 return True
     
-    return [random_car.car_id, random_move]
+#     return False
 
 
-def red_car(board):
+# def red_car_move(board):
+#     """
+#     Move red car to the right as much as possible.
+#     """
+#     red_car = board.cars['X']
+#     possibilities = red_car.get_possibilities(board)
+
+#     board.update_matrix(max(possibilities), red_car)
+
+
+def finish(board):
     """
-    Check if red car can be moved right. DOESN'T WORK!!
+    Check if red car can be moved to solution state.
     """
     red_car = board.cars['X']
     possibilities = red_car.get_possibilities(board)
 
-    if len(possibilities) > 0:
-        for i in range(len(possibilities)):
-            if possibilities[i] > 0:
-                return True
+    for move in possibilities:
+        if red_car.column + move == board.board_len - 2:
+            return True
     
     return False
 
 
-def blocking_car_move(board):
-    pass
-    
-
-def greedy_move(board):
+def finish_move(board):
     """
-    Move red car to the right as much as possible.
+    Move red car to solution state.
     """
     red_car = board.cars['X']
     possibilities = red_car.get_possibilities(board)
+    win_move = board.board_len - 2 - red_car.column
 
-    board.update_matrix(max(possibilities), red_car)
+    if win_move in possibilities:
+        board.update_matrix(win_move, red_car)
+        return [red_car, win_move]
+    else:
+        print('something is wrong')
+        print('')
+        board.print()
+        print('')
+        return randomise.random_move(board)
 
+
+# def blocking_car_move(board):
+#     pass
+    
 
 def run(board):
     """
-    Check if red car can be moved to the right and do so if possible. Otherwhise make random move. 
+    Check if greedy move is available and perform it if possible. Otherwhise make random move. 
     Continue until the board is solved.
     """
     moves_made = [['car', 'move']]
@@ -61,18 +76,31 @@ def run(board):
 
     while not board.is_solution():
 
-        if counter % 10 == 0 and red_car(board):
-            move = greedy_move(board)
+        if finish(board):
+            move = finish_move(board)
         else:
-            move = random_move(board)
+            move = randomise.random_move(board)
 
         moves_made.append(move)
-        # print(moves_made)
-        # print('')
-        # board.print()
-        # print('')
         counter += 1   
-    
 
-    return counter
     # return moves_made
+    return counter
+
+
+def print_last_steps(board, moves_by_cars):
+    last_moves = moves_by_cars[-3:]
+    print(last_moves)
+
+    for i in reversed(range(len(last_moves))):
+        print('')
+        board.print()
+        print('')
+        move_by_car = last_moves[i]
+        print(move_by_car)
+        
+        board.update_matrix(move_by_car[1] * -1, move_by_car[0])
+    
+    print('')
+    board.print()
+
