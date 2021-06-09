bca6f8920bbe0ace14f8e8d7aebd61113f3d21a2
Luc Sträter <lucstrater@gmail.com>
2021-06-09 11:38:28 +0200

    rewrote the depth first algorithm to be a class (after that was said in the live coding lecture)

---
+++ code/algorithms/depth_first.py
@@ -0,0 +1,66 @@
+import copy
+
+
+class DepthFirst():
+    """
+    A Depth First algorithm that builds a stack of boards.
+    """
+    def __init__(self, board):
+        self.board = copy.deepcopy(board)
+
+        # initialise the stack with the starting board
+        self.stack = [copy.deepcopy(self.board)]
+
+        self.archive = []
+        self.solutions = []
+        
+    def get_next_state(self):
+        """
+        Method that gets the next state from the list of states.
+        """
+        # take value 
+        return self.stack.pop()
+
+    def create_children(self, state):
+        # get current possibilities of all cars on board
+        for car in state.cars.values():
+            car_possibilities = car.get_possibilities(state)
+            
+            # for every car loop over its possible moves
+            for move in car_possibilities:
+                # copy the board of the previous board
+                new_board = copy.deepcopy(state)
+
+                # update new board with chosen move
+                new_board.update_matrix(move, new_board.cars[car.car_id])
+                
+                # append move made to list of moves to get to incumbent (new) board
+                new_board.add_move(new_board.cars[car.car_id].car_id, move)
+
+                # proofed working but needs to append matrices below
+                if new_board.matrix not in self.archive:
+                    self.stack.append(new_board)
+
+        self.archive.append(state.matrix)
+
+    def run(self):
+
+        # repeat untill the stack is empty
+        while len(self.stack) > 0:
+            # take the board at the top of the stack
+            state = self.get_next_state()
+            depth = 137
+
+            # if the current board is a solution save it
+            if state.is_solution():
+                self.solutions.append(state.moves)
+                print(state.moves)
+
+            # if length of list of moves is smaller than depth, create new boards at deeper level 
+            if len(state.moves) < depth:
+                self.create_children(state)
+
+        self.solutions.sort(key=len)
+
+        # return the best solution found
+        return self.solutions[0] 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from code.classes.car import Car
 from code.algorithms import randomise
 from code.algorithms import backtracking
 from code.algorithms import depth
+from code.algorithms import depth_first as df
 from code.output import output
 # from code.algorithms import greedy as gr
 # from code.visualisation import visualise as vis
@@ -22,8 +23,9 @@ def main():
         
     # print(f"The average amount of steps of 1 iterations is: {counters/1}")
     # print(f"The maximum steps of 1 iterations is: {maximum}")
+    depth = df.DepthFirst(rushHourBoard)
 
-    all_moves = depth.depth_run(rushHourBoard)
+    all_moves = depth.run()
 
     output.export_to_csv(all_moves, './data/output/output.csv')
     
