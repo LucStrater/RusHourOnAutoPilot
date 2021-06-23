b92b941878e3c8c44fdad3a922865eac6da16bda
Nilss98 <breeman.nb@gmail.com>
2021-06-23 11:22:56 +0200

    visualisation works with terminal prompting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from code.classes.model import Model
 from code.classes.generate import Generate
-# from code.visualisation.pygame_viz import Game
+from code.visualisation.pygame_viz import Game
 from sys import argv
 from code.algorithms import depth_first as df
 from code.algorithms import breadth_first as bf
@@ -15,6 +15,8 @@ import time
 def main():
 
     rushHourBoard = get_board()
+
+    vizBoard = rushHourBoard
 
     algorithm = get_algorithm()
     
@@ -32,6 +34,8 @@ def main():
         moves = a_star(rushHourBoard)
     elif algorithm == 'hill climber':
         moves = hillclimber(rushHourBoard)
+
+    run_visualisation(vizBoard, moves)
 
     output.export_to_csv(moves, './data/output/output.csv')
 
@@ -76,6 +80,23 @@ def get_algorithm():
             return algorithm.lower()
         
         print('Error: invalid name. Please make sure the name is spelled correctly, including any spaces.', end='\n\n')
+
+def run_visualisation(vizBoard, moves):
+    # still need to note that its only for windows
+
+    good_input = False
+    print('NOTE: Visualisation only possible when running on Windows (not on Linux/WSL).')
+    while not good_input:
+        visualise = input(' Do you want to visualise your solution? (yes/no): ')
+        visualise = visualise.lower()
+        if visualise == 'yes' or visualise == 'no':
+            good_input = True
+    
+    if visualise == 'yes':
+        print('NOTE: for visualisation, see pop-up pygame window')
+        vizBoard.moves = moves[1:]
+        viz = Game(vizBoard)
+        viz.run()
 
 
 def randomise(rushHourBoard):
@@ -213,14 +234,5 @@ def hillclimber(rushHourBoard):
 if __name__ == "__main__":
     main()
 
-
-
-    ######################## Visualisation #####################
-
-    # vizBoard = Model(board_title)
-    # vizBoard.moves = hc_moves[1:]
-    
-    # viz = Game(vizBoard)
-    # viz.run()
 
 
